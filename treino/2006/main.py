57c1985e80c4e2274253cc1e0b32393b99f8f73f
ImGabreuw <imgabreuw.dev@gmail.com>
2023-06-19 21:55:26 -0300

    Beecrowd: resolução 2006

---
+++ treino/2006/main.py
@@ -0,0 +1,6 @@
+# Execution time: 0.000s
+
+if __name__ == '__main__':
+    correct = input()
+    answers = input()
+    print(answers.count(correct))
---
+++ treino/2006/test_1.py
@@ -0,0 +1,4 @@
+if __name__ == '__main__':
+    correct = input()
+    answers = input()
+    print(answers.count(correct))
