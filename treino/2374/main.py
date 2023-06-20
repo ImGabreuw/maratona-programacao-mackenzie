39428b7e6de100e77f24797077e4709f573904a3
ImGabreuw <imgabreuw.dev@gmail.com>
2023-06-19 22:05:26 -0300

    Beecrowd: resolução 2374

---
+++ treino/2374/main.py
@@ -0,0 +1,7 @@
+# Execution time: 0.004s
+
+if __name__ == '__main__':
+    p1 = int(input())
+    p2 = int(input())
+
+    print(p1 - p2)
---
+++ treino/2374/test_1.py
@@ -0,0 +1,5 @@
+if __name__ == '__main__':
+    p1 = int(input())
+    p2 = int(input())
+
+    print(p1 - p2)
